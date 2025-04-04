e652ac4f6bb92891ec30200d6e229399f63046ce
pedrochung <pedrohchung@gmail.com>
2025-04-03 22:25:19 -0300

    change js update

--- scripts.js
+++ scripts.js
@@ -1,25 +1,55 @@
 
 
 const convertButton = document.querySelector(".converter-button")
+const  targetSelect = document.querySelector(".target-select")
 
 function convertValues (){
     const inputValue = document.querySelector(".input-value").value
-    const baseValue = document.querySelector(".base-value")
     const targetValue = document.querySelector(".target-value")
+    const baseValue = document.querySelector(".base-value")
 
     const dolarToday = 6
+    const ieneToday = 26
 
-    const convertedValue = inputValue / dolarToday
 
-    baseValue.innerHTML = new Intl.NumberFormat("pt-BR", {
+
+    if (targetSelect.value == "dolar") {
+        targetValue.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format( inputValue/dolarToday)
+    }
+
+    if (targetSelect.value == "iene") {
+        targetValue.innerHTML = new  Intl.NumberFormat("ja-JP", {
+            style: "currency",
+            currency: "JPY"  
+        }).format(inputValue*ieneToday)
+    }
+
+    baseValue.innerHTML = new  Intl.NumberFormat("pt-BR", {
         style: "currency",
-        currency: "BRL"
+        currency: "BRL"  
     }).format(inputValue)
-    targetValue.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD"
-    }).format(convertedValue)
-    
+
+
 }
 
+function changeValue(){
+    const textTarget = document.querySelector(".text-target")
+    const targeLogo = document.querySelector(".target-logo")
+
+    if (targetSelect.value == "dolar") {
+        textTarget.innerHTML = "Dólar americano"
+        targeLogo.src = "./assets/usd.png"
+    }
+    if (targetSelect.value == "iene") {
+        textTarget.innerHTML = "Iene Japones"
+        targeLogo.src = "./assets/iene.png"
+    }
+
+    convertValues ()
+}
+
+targetSelect.addEventListener("change", changeValue)
 convertButton.addEventListener("click", convertValues)
