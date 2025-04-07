5807fc367175461a919d65abd82b2e659271c7ee
pedrochung <pedrohchung@gmail.com>
2025-04-07 20:32:58 -0300

    Refeito para treinar de novo

--- scripts.js
+++ scripts.js
@@ -1,55 +1,54 @@
 
 
 const convertButton = document.querySelector(".converter-button")
-const  targetSelect = document.querySelector(".target-select")
+const targetSelect = document.querySelector(".target-select")
 
-function convertValues (){
+function convertValues() {
     const inputValue = document.querySelector(".input-value").value
-    const targetValue = document.querySelector(".target-value")
     const baseValue = document.querySelector(".base-value")
+    const targetValue = document.querySelector(".target-value")
+    
+    const dolar = 6
+    const iene = 25
 
-    const dolarToday = 6
-    const ieneToday = 26
-
-
-
-    if (targetSelect.value == "dolar") {
+    if (targetSelect.value === "dolar") {
         targetValue.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD"
-        }).format( inputValue/dolarToday)
+        }).format(inputValue / dolar)
     }
+    
+    if (targetSelect.value ==="iene"){
 
-    if (targetSelect.value == "iene") {
-        targetValue.innerHTML = new  Intl.NumberFormat("ja-JP", {
+        targetValue.innerHTML = new Intl.NumberFormat("jp-JA", {
             style: "currency",
-            currency: "JPY"  
-        }).format(inputValue*ieneToday)
+            currency: "JPY"
+        }).format(inputValue * iene)
     }
 
-    baseValue.innerHTML = new  Intl.NumberFormat("pt-BR", {
+    baseValue.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
-        currency: "BRL"  
+        currency: "BRL"
     }).format(inputValue)
-
-
+ 
 }
 
-function changeValue(){
+function changeTarget (){
     const textTarget = document.querySelector(".text-target")
-    const targeLogo = document.querySelector(".target-logo")
+    const targetLogo = document.querySelector(".target-logo")
 
-    if (targetSelect.value == "dolar") {
-        textTarget.innerHTML = "Dólar americano"
-        targeLogo.src = "./assets/usd.png"
+    if (targetSelect.value === "dolar"){
+        textTarget.innerHTML = "Dólar Americano"
+        targetLogo.src = "./assets/usd.png"
     }
-    if (targetSelect.value == "iene") {
+
+    if (targetSelect.value === "iene"){
         textTarget.innerHTML = "Iene Japones"
-        targeLogo.src = "./assets/iene.png"
+        targetLogo.src = "./assets/iene.png"
     }
-
-    convertValues ()
+    convertValues()
 }
 
-targetSelect.addEventListener("change", changeValue)
+
+targetSelect.addEventListener("change", changeTarget)
 convertButton.addEventListener("click", convertValues)
